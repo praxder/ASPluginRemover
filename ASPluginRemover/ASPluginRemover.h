c19756a8e665c77d62a5c42b885f4a7cba228d7c
Adam N. Smith <adamatrr@gmail.com>
2014-01-01 17:41:36 -0700

    Initial Commit

---
+++ ASPluginRemover/ASPluginRemover.h
@@ -0,0 +1,13 @@
+//
+//  ASPluginRemover.h
+//  ASPluginRemover
+//
+//  Created by Adam N. Smith on 1/1/14.
+//  Copyright (c) 2014 Magnus Development. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface ASPluginRemover : NSObject
+
+@end
